10407139558b11e0ff005bf61cf09138df0dc05a
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-08-05 17:34:39 +0500

    API,s Working

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+// routers
+const router = require("./routes/productRouter.js");
+app.use("/api/products", router);
+
+//static Images Folder
+
+app.use("/Images", express.static("./Images"));
+
+//port
+
 const PORT = process.env.PORT || 8080;
 
 //server
