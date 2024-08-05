361ed230033f5f5c3588bc53f31706ff8403315b
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-08-05 16:09:48 +0500

    Created Models for products and reviews

--- models/index.js
+++ models/index.js
@@ -29,8 +29,23 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+db.products = require("./productModel.js")(sequelize, DataTypes);
+db.reviews = require("./reviewModel.js")(sequelize, DataTypes);
+
 db.sequelize.sync({ force: false }).then(() => {
   console.log("yes re-sync done!");
+});
+
+// 1 to Many Relation
+
+db.products.hasMany(db.reviews, {
+  foreignKey: "product_id",
+  as: "review",
+});
+
+db.reviews.belongsTo(db.products, {
+  foreignKey: "product_id",
+  as: "product",
 });
 
 module.exports = db;
