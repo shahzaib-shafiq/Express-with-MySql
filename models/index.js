41a009e28f48982da0faaca2bb738fbf47f61c5a
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-08-05 16:05:52 +0500

    Authenticated with Sequelize

---
+++ models/index.js
@@ -0,0 +1,36 @@
+const dbConfig = require("../config/dbconfig.js");
+
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("connected..");
+  })
+  .catch((err) => {
+    console.log("Error" + err);
+  });
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.sequelize.sync({ force: false }).then(() => {
+  console.log("yes re-sync done!");
+});
+
+module.exports = db;
